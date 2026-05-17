186acff1e5d265d9dedc100aa8ea29d5b6a95195
prachi2907-debug <prachisharma29072005@gmail.com>
2026-05-17 10:02:52 +0530

    Fixed git tracking

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { footerStyles } from "../assets/dummyStyles";
+
+export default function Footer() {
+  return (
+    <footer className={footerStyles.footer}>
+      <div className={footerStyles.container}>
+        <div className={footerStyles.copyright}>
+          © {new Date().getFullYear()} InvoiceAI · Built by Hexagon Digital
+          Services
+        </div>
+        <div className={footerStyles.links}>
+          <a href="/terms" className={footerStyles.link}>
+            Terms
+          </a>
+          <a href="/privacy" className={footerStyles.link}>
+            Privacy
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
